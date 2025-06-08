2022548d99f992c13d6709c28a8d9be3dc019173
AminAnip <muhdnuramin002@gmail.com>
2025-06-08 19:57:35 +0800

    fix: bind to 0.0.0.0 and use env PORT for Render compatibility

--- calc-be-main/constants.py
+++ calc-be-main/constants.py
@@ -1,9 +1,6 @@
-from dotenv import load_dotenv
 import os
-load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# Use default values only for local development
+SERVER_URL = os.getenv("SERVER_URL", "0.0.0.0")
+PORT = os.getenv("PORT", "10000")  # Any default is fine locally
+ENV = os.getenv("ENV", "dev")
